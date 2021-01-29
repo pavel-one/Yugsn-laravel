09b4332041d8de921ca14944c3f6c36645a96aec
Pavel Zarubin <pavel@orendat.ru>
2021-01-29 18:16:46 +0300

    Category seeder

--- database/factories/MaterialCategoryFactory.php
+++ database/factories/MaterialCategoryFactory.php
@@ -7,6 +7,18 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class MaterialCategoryFactory extends Factory
 {
+    public const CATEGORY_NAMES = [
+        'Власть',
+        'Политика',
+        'Экономика',
+        'Происшествия',
+        'Криминал',
+        'Общество',
+        'Культура',
+        'Спорт',
+        'Наука',
+    ];
+
     /**
      * The name of the factory's corresponding model.
      *
@@ -19,10 +31,26 @@ class MaterialCategoryFactory extends Factory
      *
      * @return array
      */
-    public function definition()
+    public function definition(): array
     {
+        $name = $this->getName();
+
         return [
-            //
+            'name' => $name,
+            'slug' => \Str::random(40),
         ];
+    }
+
+    public function getName(): string
+    {
+        $name = self::CATEGORY_NAMES[rand(0, count(self::CATEGORY_NAMES) - 1)];
+
+        $find = MaterialCategory::firstWhere('name', $name);
+
+        if (!$find) {
+            return $name;
+        }
+
+        return $this->getName();
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\MaterialCategory;
 use App\Models\User;
+use Database\Factories\MaterialCategoryFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +17,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          User::factory(10)
+             ->create();
+
+         MaterialCategory::factory(count(MaterialCategoryFactory::CATEGORY_NAMES))
              ->create();
     }
 }
